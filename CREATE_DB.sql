2c0d9aa48c4523c7374ff3a4c43df1254bb450b8
charlie-map <charjhall777@gmail.com>
2021-01-27 06:58:48 -0500

    db lines

---
+++ CREATE_DB.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS helper;
+CREATE DATABASE helper;
+
+USE helper;
+
+CREATE TABLE teachers {
+	id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(60) NOT NULL,
+	roomID CHAR(6),
+	myname VARCHAR(255),
+	teacherSocket CHAR(20),
+	teachExpireSocket BIGINT,
+	teacherIdentity CHAR(6),
+	roomOpen TINYINT(1),
+	PRIMARY KEY(id)
+}
+
+CREATE TABLE tokens {
+	id INT NOT NULL AUTO_INCREMENT,
+	token CHAR(36) NOT NULL,
+	expire BIGINT,
+	userSocket CHAR(20),
+	teacherUsername VARCHAR(255)
+}
+
+CREATE TABLE classrooms {
+	id INT NOT NULL AUTO_INCREMENT,
+	roomID CHAR(6) NOT NULL,
+	memberSocket CHAR(6) NOT NULL,
+	memberName VARCHAR(255),
+	queueing TINYINT(1) NOT NULL,
+	needHelp TINYINT(1) NOT NULL,
+	teacherIdentity CHAR(6),
+	PRIMARY KEY(id)
+}
